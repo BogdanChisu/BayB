8d9ee072e1e2e442825b1556a246d62784ea1f51
Bogdan Chisu <bchisu@gmail.com>
2023-10-31 06:25:13 +0200

    cart functionality expanded with quantity control functions: add, remove, delete

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,14 @@ from orders import views
 
 urlpatterns = [
     path('add_to_cart/<int:pk>/', views.add_to_cart, name='add-to-cart'),
-    path('cart_list/', views.OrderCartListView.as_view(), name='cart-list')
+    path('add_to_wishlist/<int:pk>', views.add_to_wishlist,
+         name='add-to-wishlist'),
+    path('increase_cart_quantity/<int:pk>', views.increase_cart_quantity,
+         name='increase-cart-quantity'),
+    path('decrease_cart_quantity/<int:pk>', views.decrease_cart_quantity,
+         name='decrease-cart-quantity'),
+    path('delete_from_cart/<int:pk>', views.delete_from_cart,
+         name='delete-from-cart'),
+    path('cart_list/', views.OrderCartListView.as_view(), name='cart-list'),
+    path('wish_list/', views.WishListView.as_view(), name='wish-list'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 
@@ -26,6 +27,16 @@ class OrderCartListView(ListView):
         context['grand_total'] = grand_total
         return context
 
+
+class WishListView(ListView):
+    model = OrderCart
+    template_name = 'orders/wish_list.html'
+    context_object_name = 'wish_products'
+
+    def get_queryset(self):
+        result = (OrderCart.objects.filter(wishlist_item=1,
+                                        user_id=self.request.user.id))
+        return result
 
 @login_required()
 def add_to_cart(request, pk):
@@ -52,4 +63,45 @@ def add_to_cart(request, pk):
             created_at=datetime.now()
         )
 
-    return redirect('home')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required()
+def add_to_wishlist(request, pk):
+    if OrderCart.objects.filter(user_id=request.user.id,
+                                product_id=pk,
+                                wishlist_item=True).exists():
+
+        return redirect('wish-list')
+    else:
+        OrderCart.objects.create(
+            user_id=request.user.id,
+            product_id=pk,
+            wishlist_item=True,
+            quantity=1,
+            created_at=datetime.now()
+        )
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required()
+def delete_from_cart(request, pk):
+    OrderCart.objects.filter(user_id=request.user.id, id=pk).delete()
+
+    return redirect('cart-list')
+
+def increase_cart_quantity(request, pk):
+    result = OrderCart.objects.get(id=pk)
+    quantity = result.quantity
+    quantity += 1
+    OrderCart.objects.filter(id=pk).update(quantity=quantity)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def decrease_cart_quantity(request, pk):
+    result = OrderCart.objects.get(id=pk)
+    quantity = result.quantity
+    if quantity == 1:
+        delete_from_cart(request, pk)
+    else:
+        quantity -= 1
+    OrderCart.objects.filter(id=pk).update(quantity=quantity)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
